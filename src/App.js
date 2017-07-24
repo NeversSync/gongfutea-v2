caef08a0c768a6100abb2208dbeba03cb72151d3
Keeley Hammond <VerteDinde@users.noreply.github.com>
2017-07-24 15:09:18 -0700

    Revert "Sync for Monday"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from './components/Main';
 import styled from 'styled-components';
+import './App.css';
 
 class App extends Component {
   render() {
